7d4b9f46e5721e17a3f0686428c4cfab6b529998
alamine95 <lsow313@gmail.com>
2021-08-10 16:46:06 +0000

    Auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,22 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @foreach (Auth()->user()->orders as $order)
+                        <div class="card">
+                            <div class="card-hearder">
+                                Commande passé le {{ Carbon\Carbon::parse($order->payement_created_at)->format('d/m/y à H:i')}}
+                                d'un montant de <strong>{{ getPrice($order->amount) }}</strong> 
+                            </div>
+                            <div class="card-body">
+                                <h6>Liste des produits</h6>
+                                @foreach (unserialize($order->products) as $product)
+                                    <div>Nom du produit: {{ $product[0]}}</div>
+                                    <div>Prix: {{ getPrice($product[1]) }}</div>
+                                    <div>Quantité: {{ $product[2]}}</div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
