5ee87970c9893090458f3560885f1b25da6b0b12
alamine95 <lsow313@gmail.com>
2021-08-16 00:24:02 +0000

    Gestion stock

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,6 +5,7 @@
     <div class="row no-gutters p-3 border rounded d-flex align-items-center flex-md-row mb-4 shadow-sm position-relative">
       <div class="col p-3 d-flex flex-column position-static">
         <muted class="d-inline-block mb-2 text-info">
+          <div class="badge badge-pill badge-info">{{ $stock }}</div>
           @foreach ($product->categories as $category)
               {{ $category->name }}{{ $loop->last ? '' : ', '}}
           @endforeach
@@ -12,11 +13,13 @@
         <h3 class="mb-4">{{ $product->title }}</h3>
         <span>{!! $product->description !!}</span>
         <strong class="mb-4 display-4 text-secondary">{{ $product->getPrice() }}</strong>
-        <form action="{{ route('cart.store') }}" method="POST">
-          @csrf
-          <input type="hidden" name="product_id" value="{{ $product->id }}">
-          <button type="submit" class="btn btn-success mb-2"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</button>
-        </form>
+        @if ($stock === 'Disponible')
+          <form action="{{ route('cart.store') }}" method="POST">
+            @csrf
+            <input type="hidden" name="product_id" value="{{ $product->id }}">
+            <button type="submit" class="btn btn-success mb-2"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</button>
+          </form>
+        @endif
       </div>
       <div class="col-auto d-none d-lg-block">
         <img src="{{ asset('storage/' . $product->image) }}" id="mainImage">
